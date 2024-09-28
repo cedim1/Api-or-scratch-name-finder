fc8c6698070a3c898ca719cab6bfbc09e834a0a1
cedim1 <164538920+cedim1@users.noreply.github.com>
2024-09-28 20:33:42 +0100

    Create js.js

---
+++ Engine/Maincodespilts/js.js
@@ -0,0 +1,4 @@
+ var Apithatwerun = document.getElementById('Apitextbox').value
+            open('https://api.scratch.mit.edu/users/' + Apithatwerun + '/')
+ var Apithatwerun = document.getElementById('Apitextbox2').value
+            open('https://api.scratch.mit.edu/users/' + Apithatwerun2 + '/')
